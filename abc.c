5b931e194dcdca482b1ebdc2bd47396bfc05bab4
ITE03050034 <picture010373@gmail.com>
2016-09-19 09:10:27 +0000

    Initial commit

---
+++ abc.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int q,w,e,r,t,a,s,d,f,g;
+	    
+	    printf("請輸入十個數字, 用空格隔開\n");
+	    scanf("%d %d %d %d %d %d %d %d %d %d",&q,&w,&e,&r,&t,&a,&s,&d,&f,&g);
+	    printf("您輸入的數字為:\n");
+	    printf("%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d",q,w,e,r,t,a,s,d,f,g);	    
+		
+		return 0;
+ } 
